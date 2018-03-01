d4bb4d53037cf6f2b206ee82e37d88ddaf49dbff
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2018-02-28 21:59:29 -0500

    Create test-for-sn.js

---
+++ test-for-sn.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         test-for-sn
+// @namespace    http://localhost/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Francis Veilleux-Gaboury
+// @match        file:///C:/Users/fv189884/Documents/test-for-sn-script.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
